11d998a60ab82346ac2b869ea8a8123149ccc557
SamuelHenrique <samuelhenrique89@hotmail.com>
2022-10-08 19:52:19 -0300

    primeiro exercicio terminado

---
+++ mes-da-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,28 @@
+// primeiro exercicio
+let nome = "SamuelHenrique"
+let email = "samuelhenrique89@hotmail.com"
+let profissao = "analistatecnico"
+let idade = 32
+let Lugar = "maldivas"
+
+const tipoNome = typeof(nome)
+const tipoEmail = typeof(email)
+const tipoProfissao = typeof(profissao)
+const TipoIdade = typeof(idade)
+const TipoLugar = typeof(Lugar)
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(Lugar)
+
+
+console.log(tipoNome)
+console.log(tipoEmail)
+console.log(tipoProfissao)
+console.log(TipoIdade)
+console.log(TipoLugar)
+
+
+
