bd040c8784b8025e07c98d98f74a14569a0d3b1e
SamuelHenrique <samuelhenrique89@hotmail.com>
2022-10-09 15:54:49 -0300

    excluir pasta 7

---
+++ mes-da-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -0,0 +1,13 @@
+//exercicio 6
+
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+let numero6 = 6
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+
