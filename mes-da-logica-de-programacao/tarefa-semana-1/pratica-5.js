21d9deb715ccf5702459e797a44e48e480b51aea
SamuelHenrique <samuelhenrique89@hotmail.com>
2022-10-09 15:27:55 -0300

    finalizado exercicio 5

---
+++ mes-da-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,11 @@
+// quinto exercicio
+let nome = "SamuelHenrique"
+let email = "samuelhenrique89@hotmail.com"
+let profissao = "analistatecnico"
+let idade = 32
+let Lugar = "maldivas"
+
+let mensagem = 'Olá,' + nome + ' Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ' + Lugar + '. Confirma pra gente alguns dados? A sua idade é ' + idade + ', você é ' + profissao + ' e o seu email é ' + email + '? Ficamos no aguardo. Muito obrigado e boa viagem! :D'
+console.log(mensagem)
+
+
