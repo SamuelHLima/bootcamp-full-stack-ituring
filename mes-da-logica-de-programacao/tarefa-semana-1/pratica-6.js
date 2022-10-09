5e873809092de814636dbf14a88690f42405770c
SamuelHenrique <samuelhenrique89@hotmail.com>
2022-10-09 16:05:47 -0300

    finalizado exercicio 6

--- mes-da-logica-de-programacao/tarefa-semana-1/pratica-6.js
+++ mes-da-logica-de-programacao/tarefa-semana-1/pratica-6.js
@@ -11,3 +11,24 @@ let numero8 = 8
 let numero9 = 9
 let numero10 = 10
 
+let resposta = 1 * 123
+let resposta1 = 2 * 123
+let resposta2 = 3 * 123
+let resposta3 = 4 * 123
+let resposta4 = 5 * 123
+let resposta5 = 6 * 123
+let resposta6 = 7 * 123
+let resposta7 = 8 * 123
+let resposta8 = 9 * 123
+let resposta9 = 10 * 123
+
+console.log(resposta)
+console.log(resposta1)
+console.log(resposta2)
+console.log(resposta3)
+console.log(resposta4)
+console.log(resposta5)
+console.log(resposta6)
+console.log(resposta7)
+console.log(resposta8)
+console.log(resposta9)
