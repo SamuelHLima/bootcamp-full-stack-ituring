716c8c27cb4e58236994694dbfb4e7e5ad7ae7fb
SamuelHenrique <samuelhenrique89@hotmail.com>
2022-10-12 21:35:43 -0300

    pratica 2 Ok

---
+++ mes-da-logica-de-programacao/tarefa-semana-2/pratica-2.js
@@ -0,0 +1,30 @@
+
+function ola(nome) {
+       console.log(`olá ${nome}` );
+    
+}
+
+ola("Maria!");
+
+
+function entrega(chave,RuaDasRendeiras, Tadeu) {
+
+    
+
+    console.log (`Fizemos a entrega do produto ${chave} no endereço ${RuaDasRendeiras} com sucesso!
+
+    O seu produto foi recebido por: ${Tadeu}
+    
+    Esperamos que você esteja contente com o produto. Caso contrário, fale conosco em até 7 dias para que possamos ajudar`);
+
+}
+
+entrega("chave de rodas","Rua Das Rendeiras,116 - Florianopolis", "Tadeu");
+
+function remetente(Email) {
+    console.log(`Atenciosamente,
+    ${Email}` );
+ 
+}
+
+remetente("Partsepecas@pecas.com.br");
