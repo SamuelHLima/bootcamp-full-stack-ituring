432b637a1a7c8e8f35aa34db6839b699311956ec
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-16 21:38:10 -0300

    pratica 10 Ok

---
+++ mes-da-logica-de-programacao/tarefa-semana-2/pratica-10.js
@@ -0,0 +1,13 @@
+
+let n1 = 1000;
+let n2 = 5;
+let n3 = 14;
+
+
+if(n1 > n2 && n1 > n3) {
+console.log(n1);
+} else if (n2 > n1 && n2 > n3) {
+console.log(n2);
+} else if (n3 > n1 && n3 > n2) {
+console.log(n3);
+}
