78e96e135984a689be230b42ee494e58a2f4c46d
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-17 19:55:30 -0300

    pratica 7 Ok

--- mes-da-logica-de-programacao/tarefa-semana-2/pratica-7.js
+++ mes-da-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -2,19 +2,14 @@
 //exiba na tela "Hello World!" em diferentes idiomas, para pelo menos 3 idiomas
 //se nenhum idioma for informado, exiba "Olá, Mundo!" por padrão
 
-const verificarQualIdioma = (idioma) => {
-    if (idioma === "es"){
-        console.log("Espanhol");
-    } else if (idioma === "pt") {
-        console.log ("Português");
-    } else if (idioma === "en"){
-        console.log("Inglês");
-    } else {
-        console.log("Olá,mundo!");
-      }
-    }
-
-    verificarQualIdioma("es");
-    verificarQualIdioma("pt");
-    verificarQualIdioma("en");
-    verificarQualIdioma("fr");
+function Hello(idioma){
+    if (idioma ==='pt')
+        console.log('Olá, Mundo!');
+    if (idioma ==='en')
+        console.log('Hello world!');
+    if (idioma ==='Jp')
+        console.log('Kon´nichiwa sekai!');
+        if (idioma ==='Zulu')
+        console.log('Sawubona Mhlaba');
+}
+Hello('Zulu');
