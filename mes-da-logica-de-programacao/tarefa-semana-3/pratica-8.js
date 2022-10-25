5c463bcdbd05556d9b02b0394599ea8b78d4e90b
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-25 20:24:15 -0300

    pratica  Ok

---
+++ mes-da-logica-de-programacao/tarefa-semana-3/pratica-8.js
@@ -0,0 +1,10 @@
+
+let positivo = (100 + 10);
+function somafinal() {
+    console.log ("O valor total depositado foi de: R$ "+ positivo)
+    console.log ("O valor total retirado foi de: R$ "+negativo)
+    console.log("O saldo final de sua conta foi positivo no valor de R$" +(positivo - negativo)) 
+    
+}
+let negativo = (20 + 30 + 7 + 21 + 5)
+somafinal();
