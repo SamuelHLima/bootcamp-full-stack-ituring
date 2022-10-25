48eba91b212c7cbe5463d16c16d02fdd1f78514a
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-10-25 20:07:22 -0300

    pratica 7 Ok

---
+++ mes-da-logica-de-programacao/tarefa-semana-3/pratica-5.js
@@ -0,0 +1,8 @@
+function exibelista(lista) {
+    for(let i=0; i<lista.length; i++){
+        console.log(lista.indexOf("Dexter"));;
+    }
+    return 
+}
+
+let lista = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
---
+++ mes-da-logica-de-programacao/tarefa-semana-3/pratica-7.js
@@ -0,0 +1,8 @@
+let calculo = (100 - 20 - 30 + 10 - 7 - 21 - 5)
+function somafinal() {
+    console.log("O saldo final de sua conta foi positivo no valor de R$" + calculo) 
+    
+}
+
+somafinal();
+
