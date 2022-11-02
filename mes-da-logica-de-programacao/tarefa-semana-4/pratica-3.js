36c15d72360c27cd9d6776cdc7f97580fad46b6f
Samuel Henrique <samuelhenrique89@hotmail.com>
2022-11-02 15:05:55 -0300

    :zap: Exercicio 3 Ok

---
+++ mes-da-logica-de-programacao/tarefa-semana-4/pratica-3.js
@@ -0,0 +1,7 @@
+let Series = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"];
+
+let termodeBusca ="B";
+
+let Listafiltro = Series.filter((item) => item.includes(termodeBusca));
+
+console.log(Listafiltro)
